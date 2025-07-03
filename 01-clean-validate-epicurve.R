6688637d2e6d23d50149f45d40cb6219be80b445
Andree Valle Campos <avallecam@gmail.com>
2025-07-03 20:10:33 +0100

    add entry to showcase clean validate epicurve in one action

---
+++ 01-clean-validate-epicurve.R
@@ -0,0 +1,25 @@
+library(tidyverse)
+
+dat <- rio::import(file = "https://epiverse-trace.github.io/tutorials-early/data/simulated_ebola_2.csv") %>%
+  dplyr::as_tibble()
+
+dat_dictionary <- readRDS(system.file("extdata", "test_dict.RDS", package = "cleanepi"))
+
+dat_dictionary
+
+# clean data
+dat %>%
+  cleanepi::standardize_column_names() %>%
+  cleanepi::standardize_dates(target_columns = "date_onset") %>%
+  cleanepi::clean_using_dictionary(dictionary = dat_dictionary) %>%
+  
+  # validate linelist
+  linelist::make_linelist(date_onset = "date_onset", gender = "gender") %>%
+  linelist::validate_linelist() %>%
+  linelist::tags_df() %>%
+  
+  # plot epicurve
+  incidence2::incidence(date_index = "date_onset",
+                        interval = "epiweek",
+                        groups = "gender") %>%
+  plot(fill = "gender")
